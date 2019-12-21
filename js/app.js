12b67802d66767e91b128dab89a878f6fbe670c9
Mackewinsson <Mackewinsson@gmail.com>
2019-12-21 15:23:57 -0300

    problem with event click handler fixed

--- js/app.js
+++ js/app.js
@@ -3,14 +3,14 @@ const form = document.querySelector('.form-wrapper');
 
 form.addEventListener('click', (e) =>{
     e.preventDefault();
+    let button = e.target;
+    let fieldSet = button.parentNode;
+    let emojiWindow = fieldSet.lastElementChild;
     if(e.target.tagName == 'BUTTON' && e.target.className == 'button-emoji'){
-        let button = e.target;
-        let fieldSet = button.parentNode;
-        let emojiWindow = fieldSet.lastElementChild;
-        if(emojiWindow.style.display === 'none'){
-            emojiWindow.style.display = 'flex'
+        if(emojiWindow.style.display === 'none' || emojiWindow.style.display == ''){
+            emojiWindow.style.display = 'flex';
         } else {
-            emojiWindow.style.display = 'none'
+            emojiWindow.style.display = 'none';
         };
     };
 });
