5d57be64473a3cded4479614ba818819a2a2b427
Mackewinsson <Mackewinsson@gmail.com>
2019-12-21 17:08:11 -0300

    Emoji window inserting emoji to input ok

--- js/app.js
+++ js/app.js
@@ -3,15 +3,38 @@ const form = document.querySelector('.form-wrapper');
 
 form.addEventListener('click', (e) =>{
     e.preventDefault();
-    let button = e.target;
-    let fieldSet = button.parentNode;
-    let emojiWindow = fieldSet.lastElementChild;
+    // Selects only the button element and triggers the click event to show .emoji-window
     if(e.target.tagName == 'BUTTON' && e.target.className == 'button-emoji'){
+        let button = e.target;
+        let buttonIcon = button.firstElementChild;
+        let fieldSet = button.parentNode;
+        let emojiWindow = fieldSet.lastElementChild;
         if(emojiWindow.style.display === 'none' || emojiWindow.style.display == ''){
             emojiWindow.style.display = 'flex';
+            buttonIcon.className = 'fas fa-arrow-down';
+             
         } else {
             emojiWindow.style.display = 'none';
+            buttonIcon.className = 'fas fa-smile-wink';
         };
+    };
+    if (e.target.tagName == 'SPAN'){
+
+        let span = e.target;
+        let emojiWindow = span.parentNode;
+        let fieldSet = emojiWindow.parentNode;
+        let input = fieldSet.children[1];
+        let emojiContent = span.textContent;
+        // get current text of the input
+        let value = input.value;
+        // save selection start and end position
+        let start = input.selectionStart;
+        let end = input.selectionEnd;
+        // update the value with our text inserted
+        input.value = value.slice(0, start) + emojiContent + value.slice(end);
+        // update cursor to be at the end of insertion
+        input.selectionStart = input.selectionEnd = start; // insert this at the end of this statement if needed ( + emojiContent.length)
+
     };
 });
 
